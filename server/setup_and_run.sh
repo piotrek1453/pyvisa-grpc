f3537db0cc35b842eea8f0b593af70ea4f96a901
Piotr <piotrek1453@gmail.com>
2025-07-08 13:33:27 +0200

    cleanup

--- server/setup_and_run.sh
+++ server/setup_and_run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
 
+if [ -d "venv" ]; then
+  . ./venv/bin/activate
+  python3 pyvisa_grpc_server.py
+  exit 0
+fi
+
 python3 -m virtualenv venv
 . ./venv/bin/activate
 pip install -r requirements.txt
