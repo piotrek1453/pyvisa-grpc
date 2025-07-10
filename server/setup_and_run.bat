df2aeede40fd4191eafa57fce67e525642c6639f
Piotr <piotrek1453@gmail.com>
2025-07-10 13:04:14 +0200

    script improvement

--- server/setup_and_run.bat
+++
@@ -1,1 +0,0 @@
-call python -m virtualenv venv && venv\Scripts\activate && pip install -r requirements.txt && python pyvisa_grpc_server.py
--- server/setup_venv.sh
+++ server/setup_venv.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/sh
 
 if [ -d "venv" ]; then
-  . ./venv/bin/activate
-  python3 pyvisa_grpc_server.py
   exit 0
 fi
 
 python3 -m virtualenv venv
 . ./venv/bin/activate
 pip install -r requirements.txt
-python3 pyvisa_grpc_server.py
